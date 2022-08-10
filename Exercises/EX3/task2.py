76732a9e6b21685c4d8b7c3410d56584676efd2a
felipe <fepipe22@gmail.com>
2022-08-10 19:04:02 +0200

    more python exercises

--- Exercises/EX3/task2.py
+++ Exercises/EX3/task2.py
@@ -6,17 +6,20 @@ def run():
         for i in range (number):
             print('please enter another number. ')
             print('function: ', i+1)
+
     if number == 2:
         number1 = int(input('Enter starting number: '))
         number2 = int(input('Enter stopping number: '))
         for i in range (number1, number2):
             print(i)
+
     if number == 3:
         number1 = int(input('Enter starting number: '))
         number2 = int(input('Enter stopping number: '))
         step = int(input('Enter step: '))
         for i in range (number1, number2, step ):
             print(i)
+            
 
 if __name__ == '__main__':
     run()
