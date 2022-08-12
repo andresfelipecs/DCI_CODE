aa22e70e6413c86344dc77616df09ba81f8d4a11
felipe <fepipe22@gmail.com>
2022-08-12 11:22:09 +0200

    ex5-task3 forgot smth but done !

--- Exercises/EX5/task3.py
+++ Exercises/EX5/task3.py
@@ -3,9 +3,9 @@ def run():
     for i in range(2):
         number = int(input('number to check: '))
         if number %2 == 0:
-            print(number,' is an even number! ')
+            print(number,' is an even number! ', '\n')
         else:
-            print(number, ' is an odd number! ')
-            
+            print(number, ' is an odd number! ', '\n')
+
 if __name__ == '__main__':
     run()
