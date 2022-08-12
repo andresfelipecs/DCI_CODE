553ebd4bff9c0a7e7e3701b9d79627e0b8f488de
felipe <fepipe22@gmail.com>
2022-08-12 09:21:24 +0200

    ex4 tas2  project finish

--- Exercises/EX4/task2.py
+++ Exercises/EX4/task2.py
@@ -1,20 +1,27 @@
 def run():
-    #list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
-    menu = """
-        January     February   March       April    May    June
-        July        August     September   October
-        November    December
+    list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+    #menu = """
+        #January     February   March       April    May    June
+        #July        August     September   October
+        #November    December
         
-        Input the name of month: 
-        """
-    option = str(input(menu))
+        #Input the name of month: """
+    #option = str(input(menu))
+    print('List of months: ', list)
+    #for i in list:
+    #    list = i.lower()
+    #months = list.split()
+    #months.lower()
+    option = str(input('Input the name of month: '))
     option.lower()
-    if option == 'january' or 'march'or 'may' or 'july' or 'august' or 'october' or 'december':
+    if option == 'january' or option == 'march'or option == 'may' or option == 'july' or option == 'august' or option == 'october' or option == 'december':
         print('Number of days: 31 days')
-    elif option == 'april'or 'june' or 'september' or 'november':
+    elif option == 'april'or option == 'june' or option == 'september' or  option == 'november':
         print('Number of days: 30 days')
+    elif option == 'february':
+        print('Number of days: 28 days')
     else:
-        Print('Number of days: 28 days')
+        print('Enter a correct value')
 
 
 
