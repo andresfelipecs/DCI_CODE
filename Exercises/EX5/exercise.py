de42d16e50979bbee408601387d59cf252f05fef
felipe <fepipe22@gmail.com>
2022-08-12 10:53:19 +0200

    ex5-1 first edit

---
+++ Exercises/EX5/exercise.py
@@ -0,0 +1,18 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+def run():
+    print('Hi, please enter the numbers that you want to sum: ')
+    for i in range(2):
+        number1 = int(input('First number: '))
+        number2 = int(input('Second number: '))
+        number3 = int(input('Third number: '))
+        result = number1 + number2 + number3
+        print('The sum is: ',result, '\n')
+
+
+
+
+if __name__ == '__main__':
+    run()
