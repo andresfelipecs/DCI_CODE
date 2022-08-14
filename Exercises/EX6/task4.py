b5dbd70b5e47ce8d422be46b7572230baaa7a7df
felipe <fepipe22@gmail.com>
2022-08-14 16:36:55 +0200

    ex6-4

---
+++ Exercises/EX6/task4.py
@@ -0,0 +1,26 @@
+
+print('\n program #1 \n')
+
+for x in range(3):
+    print(x)
+
+print('\n program #2 \n')
+
+for j in range(5):
+    print("This is loop number ", j)
+
+print('\n program #3 \n')
+
+x =10
+while x > 0:
+    print(x)
+    x -= 1
+
+print('\n program #4 \n')
+
+countdown = 5
+while countdown:
+    print(f"{countdown}")
+    countdown -= 1
+else:
+    print("Start!")
