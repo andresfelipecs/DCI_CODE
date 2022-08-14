9ac0f722952ef30474af29839c9a026e1ec1236e
felipe <fepipe22@gmail.com>
2022-08-14 13:09:18 +0200

    ex5-task4  !

--- Exercises/EX5/task4.py
+++ Exercises/EX5/task4.py
@@ -2,8 +2,9 @@ import math
 def run():
     print("Let's calculate the area of a circle O.O ")
     radius = float(input('Input the radius of the circle: '))
-    radius = round(radius, 2)
-    area = math.pi * (radius **2)
+    #radius = round(radius, 2)
+    #area = math.pi * (radius **2)
+    area = round(pi * radius**2, 2)
     print('The area of the circle with radius', radius, 'is: ', round(area, 2))
 
 
---
+++ Exercises/EX5/task8.py
@@ -0,0 +1,15 @@
+def run():
+    n1 = 1
+    n2 = 1
+    n3 = n1+n2
+    for i in range(6):
+        n4 = n3 + n2
+        
+   
+        
+
+
+
+
+if __name__ == '__main__':
+    run()
