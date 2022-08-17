fca8f90bb0f68a825e58f852348e1a081e53a328
felipe <fepipe22@gmail.com>
2022-08-17 14:41:32 +0200

    comprehensions square root dict

--- Exercises/EX6/task6.py
+++ Exercises/EX6/task6.py
@@ -6,6 +6,7 @@ y = int(input("Second number: "))
 
 result = x + y
 
-if 15 >= result <= 20:
+#if 15 <= result <= 20:
+if result >= 15 and result <= 20:
     result = 20
 print("Calculated sum is ", result)
