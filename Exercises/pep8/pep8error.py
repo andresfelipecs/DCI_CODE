56af854e78041f8bbad74bfe5ec077cc2b9356fe
felipe <fepipe22@gmail.com>
2022-08-18 10:13:19 +0200

    pep8 errors to be fixed

---
+++ Exercises/pep8/pep8error.py
@@ -0,0 +1,12 @@
+
+
+#-----------------------------------------------------------------------------
+
+CONStant = 100 
+total_amount = 5000
+lower_amount_that_we_want_to_know_the_percentage_of=50 
+percentage = lower_amount_that_we_want_to_know_the_percentage_of*CONStant / total_amount 
+
+#-----------------------------------------------------------------------
+# Hire we write a code that should have a certain limited length in order to makes it possible to have several files open side by side, etc.  
+
