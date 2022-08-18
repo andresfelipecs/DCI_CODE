38670d7977d6b6d5c6db555e290a26a941aa05c6
felipe <fepipe22@gmail.com>
2022-08-18 13:12:21 +0200

    pep8 errors to be fixed

---
+++ Exercises/pep8/pep8error2.py
@@ -0,0 +1,13 @@
+
+#fix the errors
+countries_Population={
+        'Germany':84350776,
+        'Colombia':51147125,
+        'United states':332403650,
+    }
+
+for country,population in countries_Population.items():
+        print(country +
+         ' has ' +
+          str(population) 
+          + ' population ')
