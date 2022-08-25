48f31672210d91837b20cb39c953d8c8f26302cd
felipe <fepipe22@gmail.com>
2022-08-25 12:40:32 +0200

    stringmethods ex7 upper and lower

---
+++ stringmethods/ex7/ex7.py
@@ -0,0 +1,7 @@
+
+
+original_string = 'The Quick BroWnFoX jumps over the laZy Dog!'
+print('Original String:     ' + original_string)
+print('String in uppercase: ' + original_string.upper())
+print('String in lowercase: ' + original_string.lower())
+
