eb1c172356c64cdc783f62476bc89ab198529d65
felipe <fepipe22@gmail.com>
2022-08-26 10:18:00 +0200

    Normalize and .isupper .join etc

---
+++ stringmethods/ex9/ex9.py
@@ -0,0 +1,14 @@
+def run():
+    string = input(str('Enter string: '))
+    if string.isupper():
+        string = string.lower() + string.join('!')
+        string = string.capitalize() 
+        print(string)
+    else:
+        string = string.capitalize()
+        print(string)
+
+
+
+if __name__ == '__main__':
+    run()
