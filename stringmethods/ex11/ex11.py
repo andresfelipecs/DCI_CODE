2fc755d9429c6eee523998d5a4b0538cf8977f90
felipe <fepipe22@gmail.com>
2022-08-26 10:39:45 +0200

    Done

---
+++ stringmethods/ex11/ex11.py
@@ -0,0 +1,7 @@
+def run():
+    string = "A dogmatic dog buys dogecoin to become rich and buy hotdogs every day."
+    print(f"{string} --> Output: {string.replace(' dog ', ' cat ')}")
+
+
+if __name__ == '__main__':
+    run()
