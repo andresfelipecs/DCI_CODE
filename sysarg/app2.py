f1ba332ee14cab70111e3808d9afefdad241bb9b
Andres felipe Castro salazar <fepipe22@gmail.com>
2022-09-09 09:58:17 +0200

    app1 and app2 finish

--- sysarg/app2.py
+++ sysarg/app2.py
@@ -1,35 +1,35 @@
-import argparse
-import sys
+import argparse # import argparse library
+import sys # import sys library
 
-def run():
+#def run(): # call the function
+    # init the parser
+parser = argparse.ArgumentParser(description='The application in this file will read the command line options using argparse.')
+    #addding arguments or options
+parser.add_argument('--f', type=str, help='first name of user', default='Hanson')
+parser.add_argument('--l', type=str, help='last name of user', default='Lary')
+parser.add_argument('--a', type=int, help='age name of user', default=100)
+parser.add_argument('--fast', action="store_true", help='fast mode')
 
-    parser = argparse.ArgumentParser(description='The application in this file will read the command line options using argparse.')
 
-    parser.add_argument('--f', type=str, help='first name of user')
-    parser.add_argument('--l', type=str, help='last name of user')
-    parser.add_argument('--a', type=int, help='age name of user')
-    parser.add_argument('--fast', action="store_true", help='fast mode')
+args = parser.parse_args()
 
+    #argumentList = sys.argv[1:]
+    # default var
+    # if args.a == None:
+    #     args.a = 100
 
-    args = parser.parse_args()
+    # if args.l == None:
+    #     args.l = 'Hanson'
 
-    argumentList = sys.argv[1:]
+    # if args.f == None:
+    #     args.f = 'Larry'
 
-    if args.a == None:
-        args.a = 100
-
-    if args.l == None:
-        args.l = 'Hanson'
-
-    if args.f == None:
-        args.f = 'Larry'
-
-    if args.fast:
-        print('\n', 'Hello ', args.f, args.l, '\n', 'I see that you have had', str(args.a+1), ' birthdays.', '\n')
-        print('fast mode enabled', '\n')
+if args.fast:
+    print('\n', 'Hello ', args.f, args.l, '\n', 'I see that you have had', str(args.a+1), ' birthdays.', '\n')
+    print('fast mode enabled', '\n')
             
-    else:    
-        print('\n', 'Hello ', args.f, args.l, '\n', 'I see that you have had', str(args.a+1), ' birthdays.', '\n')
+else:    
+    print('\n', 'Hello ', args.f, args.l, '\n', 'I see that you have had', str(args.a+1), ' birthdays.', '\n')
 
-if __name__ == '__main__':
-    run()
+# if __name__ == '__main__':
+#     run()
