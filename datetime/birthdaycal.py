52c695f93eb4469c66b12009fb346df0ed4e6474
Andres felipe Castro salazar <fepipe22@gmail.com>
2022-09-21 22:37:41 +0200

    birthday exercise done

--- datetime/birthdaycal.py
+++ datetime/birthdaycal.py
@@ -1,19 +1,19 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 def birthday():
 
-    birthday_date = input('\n Enter your birthday (exp: 1995 04 22) : \n ')
+    birthday_date = input('\n Enter your birthday (exp: YYYY MM DD) : \n ')
     #birthday_date = '1995 04 22'
     p = datetime.strptime(birthday_date, "%Y %m %d")
     time_now = datetime.now()
     diff = time_now - p
     years = diff.days / 365
-    months = diff.days / 31
+    months = years * 12
     weeks = diff.days / 7
     #print(diff)
 
-    print( f' Since {birthday_date} have passed: \n {int(years)} years {int(months)} months {int(weeks)} weeks {diff.days} days \n')
+    print( f'\n Since {birthday_date} have passed: \n {years} years, {months} months, {weeks} weeks, and {diff.days} days \n')
 
 if __name__ == "__main__":
     birthday()
