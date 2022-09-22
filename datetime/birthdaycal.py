0036c7468250f50694329e34ec6ea0eb615a1c7f
Andres felipe Castro salazar <fepipe22@gmail.com>
2022-09-22 14:34:40 +0200

    datetime module updated with leap year

--- datetime/birthdaycal.py
+++ datetime/birthdaycal.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def birthday():
@@ -8,6 +8,30 @@ def birthday():
     c = datetime.strptime(birthday_date, "%Y %m %d") # convert input into daytime object
     time_now = datetime.now() # set a variable with the current date 
     diff = time_now - c # find the difference between the current time and the birthday
+    print(diff, type(diff))
+    
+    #lets add the leap years into the counting :D
+    y_1 = c.year
+    y_2 = time_now.year
+
+    years_in_range = []
+    while y_1 <= y_2:
+        years_in_range.append(y_1)
+        y_1 += 1
+
+    for year in years_in_range:
+        if year %4 == 0:
+            if year %100 == 0:
+                if year %400 == 0:
+                    diff += timedelta(days=1)
+                else:
+                    diff = diff
+            else:
+                diff += timedelta(days=1) 
+        else:
+            diff = diff
+
+
     years = diff.days / 365 # the difference in days / 365 gives us the year
     months = years * 12 # the years multiply by 12 gives us the months 
     weeks = diff.days / 7 # and the difference in days divided by 7 gives us the weeks
--- datetime/eventday.py
+++ datetime/eventday.py
@@ -6,7 +6,8 @@ def event():# define function event
     event_date = input("Enter the event date (YYYY MM DD):   ") # enter the date
     c = datetime.strptime(event_date, "%Y %m %d") # convert the from str to datetime object
     time_now = datetime.now() # set a variable with the current date 
-    diff = c - time_now # find the difference between the current time and the birthday
+    diff = c - time_now # find the difference between the current time and the event  
+    print(diff, type(diff))
     years = diff.days / 365 # the difference in days / 365 gives us the year
     months = years * 12 # the years multiply by 12 gives us the months 
     weeks = diff.days / 7 # and the difference in days divided by 7 gives us the weeks
@@ -17,14 +18,3 @@ def event():# define function event
 if __name__ == "__main__":
     event()
 
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    event()
