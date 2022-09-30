89be63d48bbe87f55015a1b78c0e6cd70b101c1b
felipe <fepipe22@gmail.com>
2022-09-30 09:55:28 +0200

    index access and join method exm

---
+++ letsjoin/indexvalue.py
@@ -0,0 +1,6 @@
+
+
+lists = [ ["John",[ {"name": "Mary"} ], "Amy"], [ 32, 43,{'age': 100}, 51] ]
+
+print(lists[0][1][0]['name'])
+print(lists[1][2]['age'])
---
+++ letsjoin/letsjoinn.py
@@ -0,0 +1,8 @@
+
+transformer = ['a', 'u', 't', 'o', 'b', 'o', 't', 's']
+
+#string = ''
+
+string = ''.join(transformer)
+
+print(string)
