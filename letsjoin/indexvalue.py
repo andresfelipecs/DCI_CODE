45f324265bb6034a575fc5ff0138276f06c1b9f0
felipe <fepipe22@gmail.com>
2022-09-30 10:30:01 +0200

    index access and join method exm

--- letsjoin/indexvalue.py
+++ letsjoin/indexvalue.py
@@ -4,3 +4,14 @@ lists = [ ["John",[ {"name": "Mary"} ], "Amy"], [ 32, 43,{'age': 100}, 51] ]
 
 print(lists[0][1][0]['name'])
 print(lists[1][2]['age'])
+
+dic = {'hole':'hello', 'ggg':'hhh'}
+
+for k,v in dic.items():
+    print(k, v)
+
+for k in dic.keys():
+    print(k)
+
+for v in dic.values():
+    print(v)
